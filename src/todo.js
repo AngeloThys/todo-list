8e2f882bf21daaa798615a7101e714f9b5bfce02
Angelo Thys <thys.angelo@gmail.com>
2023-11-04 14:50:52 +0000

    Refactor todo factory function, Add identifier to todo

--- src/todo.js
+++ src/todo.js
@@ -1,36 +1,28 @@
 // Factory Function that produces todo objects
-const todoFactory = function () {
+export default function todoFactory(identifier) {
+  const id = identifier;
   let title;
   let description;
   let dueDate;
   let priority;
   let status;
 
-  let setTitle = (newTitle) => (title = newTitle);
-  let getTitle = () => title;
-
-  let setDescription = (newDescription) => (description = newDescription);
-  let getDescription = () => description;
-
-  let setDueDate = (newDueDate) => (dueDate = newDueDate);
-  let getDueDate = () => dueDate;
-
-  let setPriority = (newPriority) => (priority = newPriority);
-  let getPriority = () => priority;
-
-  let setStatus = (newStatus) => (status = newStatus);
-  let getStatus = () => status;
-
   return {
-    setTitle,
-    getTitle,
-    setDescription,
-    getDescription,
-    setDueDate,
-    getDueDate,
-    setPriority,
-    getPriority,
-    setStatus,
-    getStatus,
+    getId: () => id,
+
+    setTitle: (newTitle) => (title = newTitle),
+    getTitle: () => title,
+
+    setDescription: (newDescription) => (description = newDescription),
+    getDescription: () => description,
+
+    setDueDate: (newDueDate) => (dueDate = newDueDate),
+    getDueDate: () => dueDate,
+
+    setPriority: (newPriority) => (priority = newPriority),
+    getPriority: () => priority,
+
+    setStatus: (newStatus) => (status = newStatus),
+    getStatus: () => status,
   };
-};
+}
