be97b17fafa142dc8ab1ea540660127aa61338e0
Angelo Thys <thys.angelo@gmail.com>
2023-11-06 09:44:27 +0000

    Refactor project factory, add data validation to setters

--- src/project.js
+++ src/project.js
@@ -1,23 +1,65 @@
 // Factory function that produces projects
-export default function projectFactory(identifier) {
-  const id = identifier;
+export default function projectFactory() {
+  const id = uniqueIdGenerator();
   let name;
   let color;
   const todoList = [];
 
+  const getId = () => id;
+
+  const setName = (newName) => {
+    if (newName.length > 1 && newName.length < 26) {
+      return (name = String(newName));
+    }
+
+    return false;
+  };
+  const getName = () => name;
+
+  const setColor = (newColor) => {
+    //hex color regex pattern
+    const hexColors = /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/;
+    if (hexColors.test(newColor)) {
+      return (color = String(newColor));
+    }
+
+    return false;
+  };
+  const getColor = () => color;
+
+  const addTodo = (newTodo) => {
+    if (newTodo.isA === 'todo') {
+      return todoList.push(newTodo);
+    }
+
+    return false;
+  };
+  const removeTodo = (oldTodo) => {
+    if (oldTodo.isA === 'todo') {
+      const index = todoList.indexOf(oldTodo);
+      return todoList.splice(index, 1);
+    }
+
+    return false;
+  };
+
   return {
-    getId: () => id,
-
-    setName: (newName) => (name = newName),
-    getName: () => name,
-
-    setColor: (newColor) => (color = newColor),
-    getColor: () => color,
-
-    addTodo: (newTodo) => todoList.push(newTodo),
-    removeTodo: (todo) => {
-      const index = todoList.indexOf(todo);
-      todoList.splice(index, 1);
-    },
+    isA: 'project',
+    getId,
+    setName,
+    getName,
+    setColor,
+    getColor,
+    addTodo,
+    removeTodo,
   };
 }
+
+function uniqueNumberGenerator() {
+  let id = 0;
+
+  return () => id++;
+}
+
+// We create an id generator.
+const uniqueIdGenerator = uniqueNumberGenerator();
