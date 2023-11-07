81ac285d5c9404683d715f3babdf316c399ee249
Angelo Thys <thys.angelo@gmail.com>
2023-11-07 17:07:03 +0000

    Add logic that selects which view to present

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import setEmptyView from './views/emptyView';
+import setTodoView from './views/todoView';
+
+export default function setView(project) {
+  const todos = project.getTodoList();
+  if (todos.length === 0) {
+    setEmptyView();
+  } else {
+    setTodoView(project);
+  }
+}
