b848794e2997f24e467b38fe567bd74625cfebcf
Angelo Thys <thys.angelo@gmail.com>
2023-11-10 13:17:21 +0000

    Add setDefaultView function

--- src/components/main.js
+++ src/components/main.js
@@ -2,12 +2,22 @@ import setEmptyView from '../views/emptyView';
 import setTodoView from '../views/todoView';
 import * as filters from '../filters';
 
-export default function setView(project) {
+export function setView(project) {
   const todoList = filters.getProjectTodos(project.getId());
-  
+
   if (todoList.length === 0) {
     setEmptyView();
   } else {
     setTodoView(project);
   }
+}
+
+export function setDefaultView() {
+  const projectList = storage.getProjectsValue();
+  const defaultProjectId = 0;
+  const defaultProject = projectList.find(
+    (project) => project.getId() === defaultProjectId
+  );
+
+  setView(defaultProject);
 }
