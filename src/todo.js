d0ba6ab2b2e100647a673bc9638e10cfd67ef3a8
Angelo Thys <thys.angelo@gmail.com>
2023-11-10 17:42:58 +0000

    Separate todoFactory from todoFunctions to repopulate said functions after retrieval from localStorage Change `this` implementation

--- src/todo.js
+++ src/todo.js
@@ -1,87 +1,105 @@
 import { isDate } from 'date-fns';
 import { idGenerator } from './helpers';
 
-// Factory Function that produces todo objects
-export default function todoFactory() {
-  const id = idGeneratorOne();
-  let projectId;
-  let title;
-  let description;
-  let dueDate;
-  let priority;
-  let status;
-
-  const getId = () => id;
-
-  const setProjectId = (newProjectId) => {
-    if (Number.isInteger(newProjectId)) {
-      return (projectId = newProjectId);
-    }
-
-    return false;
-  }
-  const getProjectId = () => projectId;
-
-  const setTitle = (newTitle) => {
-    if (newTitle.length > 1 && newTitle.length < 26) {
-      return (title = newTitle);
-    }
-
-    return false;
-  };
-  const getTitle = () => title;
-
-  const setDescription = (newDescription) => {
-    return (description = newDescription);
-  };
-  const getDescription = () => description;
-
-  const setDueDate = (newDueDate) => {
-    if (isDate(newDueDate)) {
-      return (date = newDueDate);
-    }
-
-    return false;
-  };
-  const getDueDate = () => dueDate;
-
-  const setPriority = (newPriority) => {
-    switch (newPriority) {
-      case 1:
-      case 2:
-      case 3:
-        return (priority = newPriority);
-      default:
-        return false;
-    }
-  };
-  const getPriority = () => priority;
-
-  const setStatus = (newStatus) => {
-    if (newStatus instanceof Boolean) {
-      return (status = newStatus);
-    }
-
-    return false;
-  };
-  const getStatus = () => status;
-
-  return {
-    isA: 'todo',
-    getId,
-    setProjectId,
-    getProjectId,
-    setTitle,
-    getTitle,
-    setDescription,
-    getDescription,
-    setDueDate,
-    getDueDate,
-    setPriority,
-    getPriority,
-    setStatus,
-    getStatus,
-  };
+export function todoFactory() {
+  return Object.assign(
+    {
+      isA: 'todo',
+      _id: idGeneratorOne(),
+    },
+    todoFunctions
+  );
 }
 
 const idGeneratorOne = idGenerator();
+
+function getId() {
+  return this._id;
+}
+
+function setProjectId(newProjectId) {
+  if (Number.isInteger(newProjectId)) {
+    return (this._projectId = newProjectId);
+  }
+
+  return false;
+}
+
+function getProjectId() {
+  return this._projectId;
+}
+
+function setTitle(newTitle) {
+  if (newTitle.length > 1 && newTitle.length < 26) {
+    return (this._title = newTitle);
+  }
+
+  return false;
+}
+
+function getTitle() {
+  return this._title;
+}
+
+function setDescription(newDescription) {
+  return (this._description = newDescription);
+}
+
+function getDescription() {
+  return this._description;
+}
+
+function setDueDate(newDueDate) {
+  if (isDate(newDueDate)) {
+    return (this._date = newDueDate);
+  }
+
+  return false;
+}
+
+function getDueDate() {
+  return this._dueDate;
+}
+
+function setPriority(newPriority) {
+  switch (newPriority) {
+    case 1:
+    case 2:
+    case 3:
+      return (this._priority = newPriority);
+    default:
+      return false;
+  }
+}
+
+function getPriority() {
+  return this._priority;
+}
+
+function setStatus(newStatus) {
+  if (newStatus instanceof Boolean) {
+    return (this._status = newStatus);
+  }
+
+  return false;
+}
+
+function getStatus() {
+  return this._status;
+}
+
+export const todoFunctions = {
+  getId,
+  setProjectId,
+  getProjectId,
+  setTitle,
+  getTitle,
+  setDescription,
+  getDescription,
+  setDueDate,
+  getDueDate,
+  setPriority,
+  getPriority,
+  setStatus,
+  getStatus,
+};
