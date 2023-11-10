200fc260d0fb93ac542bfd8a9cb126d9aae4ed05
Angelo Thys <thys.angelo@gmail.com>
2023-11-10 11:36:27 +0000

    Modify setView to comply with current todoList retrieval method

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,11 @@
 import setEmptyView from '../views/emptyView';
 import setTodoView from '../views/todoView';
+import * as filters from '../filters';
 
 export default function setView(project) {
-  const todos = project.getTodoList();
-  if (todos.length === 0) {
+  const todoList = filters.getProjectTodos(project.getId());
+  
+  if (todoList.length === 0) {
     setEmptyView();
   } else {
     setTodoView(project);
