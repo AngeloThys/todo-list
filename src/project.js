c41c435378e3a0ec39df7742a29a6d3a03066dee
Angelo Thys <thys.angelo@gmail.com>
2023-11-10 15:22:04 +0000

    Separate projectFactory from projectFunctions to repopulate said functions after retrieval from localStorage Change `this` implementation

--- src/project.js
+++ src/project.js
@@ -1,41 +1,54 @@
 import { idGenerator } from './helpers';
 
 // Factory function that produces projects
-export default function projectFactory() {
-  const id = idGeneratorOne();
-  let name;
-  let hexColor;
-
-  const getId = () => id;
-
-  const setName = (newName) => {
-    if (newName.length > 1 && newName.length < 26) {
-      return (name = String(newName));
-    }
-
-    return false;
-  };
-  const getName = () => name;
-
-  const setHexColor = (newColor) => {
-    //hex color regex pattern
-    const hexColors = /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/;
-    if (hexColors.test(newColor)) {
-      return (hexColor = String(newColor));
-    }
-
-    return false;
-  };
-  const getHexColor = () => hexColor;
-
-  return {
-    isA: 'project',
-    getId,
-    setName,
-    getName,
-    setHexColor,
-    getHexColor,
-  };
+export function projectFactory() {
+  return Object.assign(
+    {
+      isA: 'project',
+      _id: idGeneratorOne(),
+      _name: '',
+      _hexColor: '',
+    },
+    projectFunctions
+  );
 }
 
 const idGeneratorOne = idGenerator();
+
+function getId() {
+  return this._id;
+}
+
+function setName(newName) {
+  if (newName.length > 1 && newName.length < 26) {
+    return (this._name = String(newName));
+  }
+
+  return false;
+}
+
+function getName() {
+  return this._name;
+}
+
+function setHexColor(newColor) {
+  //hex color regex pattern
+  const hexColors = /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/;
+  if (hexColors.test(newColor)) {
+    return (this._hexColor = String(newColor));
+  }
+
+  return false;
+}
+
+function getHexColor() {
+  return this._hexColor;
+}
+
+export const projectFunctions = {
+  getId,
+  setName,
+  getName,
+  setHexColor,
+  getHexColor,
+};
