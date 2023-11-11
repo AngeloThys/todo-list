3b20e5260526cba8355a416cce3ed8f7b886d075
Angelo Thys <thys.angelo@gmail.com>
2023-11-11 11:23:58 +0000

    Extract reusable components from todoView to components

---
+++ src/components/components.js
@@ -0,0 +1,179 @@
+export function createAddTodoButton() {
+  const addTodoButton = document.createElement('button');
+  const createTodoDialog = document.querySelector('.createTodo');
+
+  addTodoButton.className = 'addTodo';
+  addTodoButton.addEventListener('click', () => {
+    createTodoDialog.showModal();
+  });
+
+  return addTodoButton;
+}
+
+export function createTodoContainer(todo) {
+  const todoContainer = document.createElement('div');
+
+  todoContainer.className = 'todoContainer';
+
+  return todoContainer;
+}
+
+export function createStatusInput(todo) {
+  const statusInput = document.createElement('input');
+  const todoStatus = todo.getStatus();
+
+  statusInput.type = 'checkbox';
+  statusInput.className = 'updateStatus';
+  statusInput.checked = todoStatus;
+  statusInput.addEventListener('change', () => {
+    updateTodoStatus();
+  });
+
+  return statusInput;
+}
+
+function updateTodoStatus(status, todo) {
+  todo.setStatus(status);
+  storage.updateTodo(todo);
+}
+
+export function createDueDateInput(todo) {
+  const dueDateInput = document.createElement('input');
+  const todoDueDate = todo.getDueDate();
+
+  dueDateInput.type = 'date';
+  dueDateInput.className = 'updateDueDate';
+  dueDateInput.value = todoDueDate;
+  dueDateInput.addEventListener('change', () => {
+    let date = dueDateInput.value;
+    updateTodoDueDate(date, todo);
+  });
+
+  return dueDateInput;
+}
+
+function updateTodoDueDate(date, todo) {
+  todo.setDueDate(date);
+  storage.updateTodo(todo);
+}
+
+export function createDescriptionP(todo) {
+  const descriptionP = document.createElement('p');
+  const modifyTodoDialog = document.querySelector('.modifyTodo');
+
+  descriptionP.className = 'descriptionTodo';
+  descriptionP.textContent = todo.getDescription();
+  descriptionP.addEventListener('click', () => {
+    modifyTodoDialog.showModal();
+  });
+
+  return descriptionP;
+}
+
+export function createTitleInput(todo) {
+  const titleInput = document.createElement('input');
+  const todoTitle = todo.getTitle();
+
+  titleInput.type = 'text';
+  titleInput.className = 'updateTitle';
+  titleInput.value = todoTitle;
+  // we could swap out keyup for change as change is processed before click
+  let keyupTimeout;
+  titleInput.addEventListener('keyup', () => {
+    clearTimeout(keyupTimeout);
+    let title = titleInput.value;
+    keyupTimeout = setTimeout(updateTodoTitle(title, todo), 2000);
+  });
+
+  return titleInput;
+}
+
+function updateTodoTitle(title, todo) {
+  todo.setTitle(title);
+  storage.updateTodo(todo);
+}
+
+export function createDeleteButton(project, todo) {
+  const deleteButton = document.createElement('button');
+  const deleteIcon = document.createElement('img');
+
+  deleteIcon.src = url('../images/delete.svg');
+
+  deleteButton.className = 'deleteTodo';
+  deleteButton.appendChild(deleteIcon);
+  deleteButton.addEventListener('click', () => {
+    storage.deleteTodo(todo);
+    setTodoView(project);
+  });
+
+  return deleteButton;
+}
+
+export function createExpandButton() {
+  const expandButton = document.createElement('button');
+  const expandIcon = document.createElement('img');
+  const modifyTodoDialog = document.querySelector('.modifyTodo');
+
+  expandIcon.src = url('../images/maximize.svg');
+
+  expandButton.className = 'openTodo';
+  expandButton.appendChild(expandIcon);
+  expandButton.addEventListener('click', () => {
+    modifyTodoDialog.showModal();
+  });
+
+  return expandButton;
+}
+
+export function createPriorityButton(todo) {
+  const priorityButton = document.createElement('button');
+
+  priorityButton.className = 'changePriority';
+  priorityButton.appendChild(createPriorityIcon(todo));
+  priorityButton.addEventListener('click', () => {
+    cyclePriority(todo);
+    priorityButton.replaceChildren(createPriorityIcon(todo));
+  });
+
+  return priorityButton;
+}
+
+function cyclePriority(todo) {
+  const currentPriority = todo.getPriority();
+
+  switch (currentPriority) {
+    case 1:
+      updateTodoPriority(2, todo);
+      break;
+    case 2:
+      updateTodoPriority(3, todo);
+      break;
+    case 3:
+      updateTodoPriority(1, todo);
+      break;
+  }
+}
+
+function updateTodoPriority(priority, todo) {
+  todo.setPriority(priority);
+  storage.updateTodo(todo);
+}
+
+export function createPriorityIcon(todo) {
+  const priorityIcon = document.createElement('img');
+  const currentPriority = todo.getPriority();
+
+  switch (currentPriority) {
+    case 1:
+      priorityIcon.src = url('./src/images/priority1.svg');
+      break;
+    case 2:
+      priorityIcon.src = url('./src/images/priority2.svg');
+      break;
+    case 3:
+      priorityIcon.src = url('./src/images/priority3.svg');
+      break;
+  }
+
+  return priorityIcon;
+}
--- src/views/todoView.js
+++ src/views/todoView.js
@@ -1,29 +1,18 @@
 import * as storage from '../storage.js';
 import * as filters from '../filters.js';
+import * as components from '../components/components.js';
 
 export default function setTodoView(project) {
   const main = document.querySelector('main');
   const todoView = document.createElement('div');
   const todoElementList = createTodoElementList(project);
-  const addTodoButton = createAddTodoButton();
+  const addTodoButton = components.createAddTodoButton();
 
   todoView.className = 'todoView';
   todoView.replaceChildren(todoElementList);
   todoView.appendChild(addTodoButton);
 
   main.replaceChildren(todoView);
-}
-
-function createAddTodoButton() {
-  const addTodoButton = document.createElement('button');
-  const createTodoDialog = document.querySelector('.createTodo');
-
-  addTodoButton.className = 'addTodo';
-  addTodoButton.addEventListener('click', () => {
-    createTodoDialog.showModal();
-  });
-
-  return addTodoButton;
 }
 
 function createTodoElementList(project) {
@@ -38,14 +27,14 @@ function createTodoElementList(project) {
 }
 
 function createTodoElement(project, todo) {
-  const todoContainer = createTodoContainer(todo);
-  const priorityButton = createPriorityButton(todo);
-  const expandButton = createExpandButton();
-  const deleteButton = createDeleteButton(project, todo);
-  const titleInput = createTitleInput(todo);
-  const descriptionP = createDescriptionP(todo);
-  const dueDateInput = createDueDateInput(todo);
-  const statusInput = createStatusInput(todo);
+  const todoContainer = components.createTodoContainer(todo);
+  const priorityButton = components.createPriorityButton(todo);
+  const expandButton = components.createExpandButton();
+  const deleteButton = components.createDeleteButton(project, todo);
+  const titleInput = components.createTitleInput(todo);
+  const descriptionP = components.createDescriptionP(todo);
+  const dueDateInput = components.createDueDateInput(todo);
+  const statusInput = components.createStatusInput(todo);
 
   todoContainer.replaceChildren(
     priorityButton,
@@ -58,172 +47,4 @@ function createTodoElement(project, todo) {
   );
 
   return todoContainer;
-}
-
-function createTodoContainer(todo) {
-  const todoContainer = document.createElement('div');
-
-  todoContainer.className = 'todoContainer';
-
-  return todoContainer;
-}
-
-function createStatusInput(todo) {
-  const statusInput = document.createElement('input');
-  const todoStatus = todo.getStatus();
-
-  statusInput.type = 'checkbox';
-  statusInput.className = 'updateStatus';
-  statusInput.checked = todoStatus;
-  statusInput.addEventListener('change', () => {
-    updateTodoStatus();
-  });
-
-  return statusInput;
-}
-
-function updateTodoStatus(status, todo) {
-  todo.setStatus(status);
-  storage.updateTodo(todo);
-}
-
-function createDueDateInput(todo) {
-  const dueDateInput = document.createElement('input');
-  const todoDueDate = todo.getDueDate();
-
-  dueDateInput.type = 'date';
-  dueDateInput.className = 'updateDueDate';
-  dueDateInput.value = todoDueDate;
-  dueDateInput.addEventListener('change', () => {
-    let date = dueDateInput.value;
-    updateTodoDueDate(date, todo);
-  });
-
-  return dueDateInput;
-}
-
-function updateTodoDueDate(date, todo) {
-  todo.setDueDate(date);
-  storage.updateTodo(todo);
-}
-
-function createDescriptionP(todo) {
-  const descriptionP = document.createElement('p');
-  const modifyTodoDialog = document.querySelector('.modifyTodo');
-
-  descriptionP.className = 'descriptionTodo';
-  descriptionP.textContent = todo.getDescription();
-  descriptionP.addEventListener('click', () => {
-    modifyTodoDialog.showModal();
-  });
-
-  return descriptionP;
-}
-
-function createTitleInput(todo) {
-  const titleInput = document.createElement('input');
-  const todoTitle = todo.getTitle();
-
-  titleInput.type = 'text';
-  titleInput.className = 'updateTitle';
-  titleInput.value = todoTitle;
-  // we could swap out keyup for change as change is processed before click
-  let keyupTimeout;
-  titleInput.addEventListener('keyup', () => {
-    clearTimeout(keyupTimeout);
-    let title = titleInput.value;
-    keyupTimeout = setTimeout(updateTodoTitle(title, todo), 2000);
-  });
-
-  return titleInput;
-}
-
-function updateTodoTitle(title, todo) {
-  todo.setTitle(title);
-  storage.updateTodo(todo);
-}
-
-function createDeleteButton(project, todo) {
-  const deleteButton = document.createElement('button');
-  const deleteIcon = document.createElement('img');
-
-  deleteIcon.src = url('../images/delete.svg');
-
-  deleteButton.className = 'deleteTodo';
-  deleteButton.appendChild(deleteIcon);
-  deleteButton.addEventListener('click', () => {
-    storage.deleteTodo(todo);
-    setTodoView(project);
-  });
-
-  return deleteButton;
-}
-
-function createExpandButton() {
-  const expandButton = document.createElement('button');
-  const expandIcon = document.createElement('img');
-  const modifyTodoDialog = document.querySelector('.modifyTodo');
-
-  expandIcon.src = url('../images/maximize.svg');
-
-  expandButton.className = 'openTodo';
-  expandButton.appendChild(expandIcon);
-  expandButton.addEventListener('click', () => {
-    modifyTodoDialog.showModal();
-  });
-
-  return expandButton;
-}
-
-function createPriorityButton(todo) {
-  const priorityButton = document.createElement('button');
-
-  priorityButton.className = 'changePriority';
-  priorityButton.appendChild(createPriorityIcon(todo));
-  priorityButton.addEventListener('click', () => {
-    cyclePriority(todo);
-    priorityButton.replaceChildren(createPriorityIcon(todo));
-  });
-
-  return priorityButton;
-}
-
-function cyclePriority(todo) {
-  const currentPriority = todo.getPriority();
-
-  switch (currentPriority) {
-    case 1:
-      updateTodoPriority(2, todo);
-      break;
-    case 2:
-      updateTodoPriority(3, todo);
-      break;
-    case 3:
-      updateTodoPriority(1, todo);
-      break;
-  }
-}
-
-function updateTodoPriority(priority, todo) {
-  todo.setPriority(priority);
-  storage.updateTodo(todo);
-}
-
-function createPriorityIcon(todo) {
-  const priorityIcon = document.createElement('img');
-  const currentPriority = todo.getPriority();
-
-  switch (currentPriority) {
-    case 1:
-      priorityIcon.src = url('./src/images/priority1.svg');
-      break;
-    case 2:
-      priorityIcon.src = url('./src/images/priority2.svg');
-      break;
-    case 3:
-      priorityIcon.src = url('./src/images/priority3.svg');
-      break;
-  }
-
-  return priorityIcon;
 }
