b869abd3c646d751b818776693ca57e335f525de
Angelo Thys <thys.angelo@gmail.com>
2023-11-11 12:33:23 +0000

    Refactor project container logic of sidebar component

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,21 +1,37 @@
 import * as storage from '../storage';
 import { setView } from './main';
 
-export function listProjectNames() {
-  const projectListContainer = document.querySelector('.projectList');
+export function setProjectContainerContent() {
+  const projectsContainer = document.querySelector('.projects');
+  const projectsHeader = createProjectsHeader();
   const projectButtonList = createProjectButtonList();
-  projectListContainer.replaceChildren(...projectButtonList);
+  const addProjectButton = createAddProjectButton();
+
+  projectsContainer.replaceChildren(
+    projectsHeader,
+    projectButtonList,
+    addProjectButton
+  );
+}
+
+function createProjectsHeader() {
+  const projectsHeader = document.createElement('h2');
+
+  projectsHeader.textContent = 'Projects';
+
+  return projectsHeader;
 }
 
 function createProjectButtonList() {
+  const projectButtonListContainer = document.createElement('div');
   const projectList = storage.getProjectsValue();
-  let projectButtonList = [];
 
+  projectButtonListContainer.className = 'projectList';
   projectList.forEach((project) => {
-    projectButtonList.push(createProjectButton(project));
+    projectButtonListContainer.appendChild(createProjectButton(project));
   });
 
-  return projectButtonList;
+  return projectButtonListContainer;
 }
 
 function createProjectButton(project) {
@@ -28,4 +44,17 @@ function createProjectButton(project) {
   });
 
   return projectButton;
+}
+
+function createAddProjectButton() {
+  const addProjectButton = document.createElement('button');
+  const createProjectDialog = document.querySelector('.createProject');
+
+  addProjectButton.className = 'addProject';
+  addProjectButton.textContent = 'Add a project'; // this needs an svg
+  addProjectButton.addEventListener('click', () => {
+    createProjectDialog.showModal();
+  });
+
+  return addProjectButton;
 }
