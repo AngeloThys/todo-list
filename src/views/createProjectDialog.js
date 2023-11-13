5ed07518a531531438abc9570b406cc044960311
Angelo Thys <thys.angelo@gmail.com>
2023-11-13 20:19:05 +0000

    Add createProjectDialog view

---
+++ src/views/createProjectDialog.js
@@ -0,0 +1,24 @@
+import * as storage from '../storage';
+import * as sidebar from '../components/sidebar';
+import * as main from '../components/main';
+
+export function createProject() {
+  const createProjectButton = document.querySelector('.createProjectButton');
+
+  createProjectButton.addEventListener('click', () => {
+    const project = storage.createProject();
+    const projectForm = document.querySelector('.projectForm');
+    const createProject = document.querySelector('.createProject');
+    const projectName = document.querySelector('#projectName').value;
+    const projectHexColor = document.querySelector('#projectColor').value;
+
+    project.setName(projectName);
+    project.setHexColor(projectHexColor);
+
+    storage.updateProject(project);
+    projectForm.reset();
+    sidebar.setProjectContainerContent();
+    main.setView(project);
+    createProject.close();
+  });
+}
