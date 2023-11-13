46d471519655a5201a5dad704fd6fa17efc2b892
Angelo Thys <thys.angelo@gmail.com>
2023-11-13 20:21:19 +0000

    Implement createProject view function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { setDefaultView } from './components/main';
 import { setProjectContainerContent } from './components/sidebar';
 import { addCloseDialogListener } from './components/dialog';
+import { createProject } from './views/createProjectDialog';
 
 setProjectContainerContent();
 setDefaultView();
 addCloseDialogListener();
+createProject();
