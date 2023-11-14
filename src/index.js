c6add91a7ad9ffcfca3f0355fb01e4af66519528
Angelo Thys <thys.angelo@gmail.com>
2023-11-14 14:58:42 +0000

    Modify function import names of createTodoDialog module

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import { setDefaultView } from './components/main';
 import { setProjectContainerContent } from './components/sidebar';
 import { addCloseDialogListener } from './components/dialog';
 import { createProject } from './views/createProjectDialog';
+import { createTodo, addPriorityButton } from './views/createTodoDialog';
 
 setProjectContainerContent();
 setDefaultView();
 addCloseDialogListener();
 createProject();
+addPriorityButton();
+createTodo();
